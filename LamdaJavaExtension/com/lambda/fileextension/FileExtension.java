cd74c3288f1b9d07059c54a9775271b8aca4473f
Anindya96ghosh <41475961+Anindya96ghosh@users.noreply.github.com>
2018-08-04 22:05:14 +0530

    Update FileExtension.java

--- LamdaJavaExtension/com/lambda/fileextension/FileExtension.java
+++ LamdaJavaExtension/com/lambda/fileextension/FileExtension.java
@@ -6,9 +6,13 @@ public class FileExtension {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		
+		//Accepting the path from user
 		System.out.println("Enter the directory name along with the path");
 		String path=scanner.nextLine();
 		File dir=new File(path);
+		
+		//Accepting the extention from the user
 		System.out.println("Enter the extension");
 		String ext=scanner.nextLine();
 		
@@ -17,6 +21,7 @@ public class FileExtension {
 		for(String file:listFile) {
 			System.out.println(file);
 		}
+		
 		scanner.close();
 	}
 }
